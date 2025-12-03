df279c28eaf672eb68a8afa86d3d11f6dad5aad7
mangleuyy <mangleuyy@gmail.com>
2025-12-03 22:12:48 +0700

    First Commit

---
+++ src/main/java/com/mycompany/pert4_51422074/Pert4_51422074.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pert4_51422074;
+
+/**
+ *
+ * @author Hikmale
+ */
+public class Pert4_51422074 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
